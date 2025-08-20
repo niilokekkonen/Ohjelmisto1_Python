2d04c797a57bd9be938b58d10cfb3e4bda883f8d
niilokekkonen <niilo.kekkonen@metropolia.fi>
2025-08-20 11:29:01 +0300

    mod 2 initial commit

--- Mod 2/mod 2.6.py
+++ Mod 2/mod 2.6.py
@@ -1,4 +1,6 @@
 import random
 
-lukonkoodi_1 = random.randint(0,9,)
-print(lukonkoodi_1)
+lukonkoodi_1 = random.sample(range(0,9,),3)
+print(f"Ensimmäinen koodi: {lukonkoodi_1}")
+lukonkoodi_2 = random.sample(range(1,6),4)
+print(f"Toinen koodi {lukonkoodi_2}")
