aae88839276b1fa633406464ae263af1b73583d0
Niilo Kekkonen <niilo.kekkonen@metropolia.fi>
2025-09-04 00:29:51 +0300

    Updated mod 6.4.py
    Selitykset lisätty

--- Mod 6/mod 6.4.py
+++ Mod 6/mod 6.4.py
@@ -1,12 +1,12 @@
 
-def m_sum(lista):
-    total = 0
-    for num in lista:
-        total = total + num
-    return total
+def m_sum(lista): #funktio, jonka parametri on lista
+    total = 0 #Yhteisluku
+    for num in lista: # For-loop, joka käy läpi listan
+        total = total + num #Lisää muuttujaan 'total' jokaisen luvu
+    return total #Palauttaa arvon total
 
-numbers = [5,5,5,5,2]
-yht = m_sum(numbers)
-print(yht)
+numbers = [5,5,5,5,2] #Lista
+yht = m_sum(numbers) # Tallentaa funktion kutsun muuttujaan yht
+print(yht) #Tulostaa muuttujan yht
 
 
