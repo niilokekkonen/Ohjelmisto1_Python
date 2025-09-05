348ae6d054de083d9d199aa68f4ad660a5891f61
niilokekkonen <niilo.kekkonen@metropolia.fi>
2025-09-05 14:12:11 +0300

    Muutettu while-loop, funktion ulkopuolelle.

--- Mod 6/mod 6.1.py
+++ Mod 6/mod 6.1.py
@@ -2,22 +2,20 @@ import random
 
 
 def nopanheitot(): #Funktio heittää noppaa ja tulostaa nopasta tulevan luvun
-    heitto = random.randint(1,6)
-    heitot = 0
-    heitot += 1
-    while heitto != 6:
-        heitto = random.randint(1, 6) #Noppa
-        print(f"Heitto numero:{heitot} tulos:{heitto}")  # Tulosta heiton numeron ja nopasta tulleen luvun
-        heitot += 1 #Kasvattaa heiton numeroa joka kierroksella 1
-        if heitto == 6:  # Jos heitto on 6 ohjelma sammuu ja tämä tulostuu
-            print(f"Heitto numero:{heitot} tulos:{heitto}. numero 6 sammuttaa ohjelman")
-    return heitto
+    return random.randint(1,6)
 
 
+heitot = 1
+while True:
+    heitto = nopanheitot()# Kutsuu funktiota nopanheitot
+    print(f"Heitto numero:{heitot} tulos:{heitto}")  # Tulosta heiton numeron ja nopasta tulleen luvun
+    if heitto == 6:  # Jos heitto on 6 ohjelma sammuu ja tämä tulostuu
+        print(f"Numero 6 sammuttaa ohjelman")
+        heitot += 1  # Kasvattaa heiton numeroa joka kierroksella 1
+        break
+    heitot += 1 #Kasvattaa heiton numeroa joka kierroksella 1
 
 
-
-nopanheitot() #Kutsuu funktiota nopanheitot
 
 
 
--- Mod 6/mod 6.2.py
+++ Mod 6/mod 6.2.py
@@ -2,17 +2,19 @@ import random
 
 max_heitto = int(input("Mikä on nopan isoin luku?:")) #käyttäjältä saatava maksimi tulos
 
-def nopanheitot(max_heitto): #Funktio heittää noppaa ja tulostaa nopasta tulevan luvun
-    heitto = random.randint(1,max_heitto) #Nopan heitto
-    heitot = 1  # Heiton numero
-    while heitto < max_heitto:
-        print(f"Heitto numero:{heitot} tulos:{heitto}")  # Tulosta heiton numeron ja nopasta tulleen luvun
-        heitto = random.randint(1, max_heitto)
-        heitot += 1  # Kasvattaa heiton numeroa joka kierroksella yhdellä
-        if heitto == max_heitto:  # Jos heitto on 6 ohjelma sammuu ja tämä tulostuu
-            print(f"Heitto numero:{heitot} tulos:{heitto}. numero {max_heitto} sammuttaa ohjelman")
-            break
+def nopanheitot(max_heitto): #Funktio heittää noppaa ja palauttaa nopasta tulevan luvun
+    return random.randint(1,max_heitto) #Nopan heitto
+
+
+heitot = 1  # Heiton numero
+while True:
+    heitto = nopanheitot(max_heitto)
+    print(f"Heitto numero:{heitot} tulos:{heitto}")  # Tulosta heiton numeron ja nopasta tulleen luvun
+    heitot += 1  # Kasvattaa heiton numeroa joka kierroksella yhdellä
+    if heitto == max_heitto:  # Jos heitto on 6 ohjelma sammuu ja tämä tulostuu
+        print(f"Ohjelma sammuu, numero {max_heitto} sammuttaa ohjelman")
+        break
 
 
 
-nopanheitot(max_heitto)
+
